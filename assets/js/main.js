34bb140688580755135709bb049049e597040468
Buchanan Kerswell <buck.kerswell@gmail.com>
2021-02-18 06:09:33 -0700

    add auto play/pause with jquery isinview

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,16 @@
 
 (function($) {
 
+	$(window).scroll(function() {
+	    $('video').each(function(){
+	        if ($(this).is(":in-viewport")) {
+	            $(this)[0].play();
+	        } else {
+	            $(this)[0].pause();
+	        }
+	    })
+	})
+
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
